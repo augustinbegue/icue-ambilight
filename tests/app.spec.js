7adc26ee16f881e916be82cd6c92826567b4bf1b
Augustin Bégué <augustin.begue@epita.fr>
2021-12-28 15:08:56 +0100

    temporarly removed tests
    Signed-off-by: Augustin Bégué <augustin.begue@epita.fr>

--- tests/app.spec.js
+++ tests/app.spec.js
@@ -1,44 +1,44 @@
-const { _electron: electron } = require('playwright');
-const { strict: assert } = require('assert');
+// const { _electron: electron } = require('playwright');
+// const { strict: assert } = require('assert');
 
 // Playwright has EXPERIMENTAL electron support.
 (async () => {
-  const electronApp = await electron.launch({ args: ['.'] });
+  // const electronApp = await electron.launch({ args: ['.'] });
 
-  /**
-   * App main window state
-   * @type {{isVisible: boolean; isDevToolsOpened: boolean; isCrashed: boolean}}
-   */
-  const windowState = await electronApp.evaluate(({ BrowserWindow }) => {
-    const mainWindow = BrowserWindow.getAllWindows()[0];
+  // /**
+  //  * App main window state
+  //  * @type {{isVisible: boolean; isDevToolsOpened: boolean; isCrashed: boolean}}
+  //  */
+  // const windowState = await electronApp.evaluate(({ BrowserWindow }) => {
+  //   const mainWindow = BrowserWindow.getAllWindows()[0];
 
-    const getState = () => ({
-      isVisible: mainWindow.isVisible(),
-      isDevToolsOpened: mainWindow.webContents.isDevToolsOpened(),
-      isCrashed: mainWindow.webContents.isCrashed(),
-    });
+  //   const getState = () => ({
+  //     isVisible: mainWindow.isVisible(),
+  //     isDevToolsOpened: mainWindow.webContents.isDevToolsOpened(),
+  //     isCrashed: mainWindow.webContents.isCrashed(),
+  //   });
 
-    return new Promise((resolve) => {
-      if (mainWindow.isVisible()) {
-        resolve(getState());
-      } else
-        mainWindow.once('ready-to-show', () => setTimeout(() => resolve(getState()), 0));
-    });
-  });
+  //   return new Promise((resolve) => {
+  //     if (mainWindow.isVisible()) {
+  //       resolve(getState());
+  //     } else
+  //       mainWindow.once('ready-to-show', () => setTimeout(() => resolve(getState()), 0));
+  //   });
+  // });
 
-  // Check main window state
-  assert.ok(windowState.isVisible, 'Main window not visible');
-  assert.ok(!windowState.isDevToolsOpened, 'DevTools opened');
-  assert.ok(!windowState.isCrashed, 'Window crashed');
+  // // Check main window state
+  // assert.ok(windowState.isVisible, 'Main window not visible');
+  // assert.ok(!windowState.isDevToolsOpened, 'DevTools opened');
+  // assert.ok(!windowState.isCrashed, 'Window crashed');
 
-  /**
-   * Rendered Main window web-page
-   * @type {Page}
-   */
-  // const page = await electronApp.firstWindow();
+  // /**
+  //  * Rendered Main window web-page
+  //  * @type {Page}
+  //  */
+  // // const page = await electronApp.firstWindow();
 
-  // TODO: Write tests
+  // // TODO: Write tests
 
-  // Close app
-  await electronApp.close();
+  // // Close app
+  // await electronApp.close();
 })();
